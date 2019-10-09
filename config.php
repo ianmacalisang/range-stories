2ebdaba3f52d73db86e6602b7f41aee6a2e890b4
Ian Andrew Macalisang <39157186+ianmacalisang@users.noreply.github.com>
2019-10-09 15:46:37 +0400

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', '85.187.128.12');
+define('DB_SERVER', '85.187.128.12:3306');
 define('DB_USERNAME', 'xpertise_dev');
 define('DB_PASSWORD', 'xpertise!@#$');
 define('DB_NAME', 'xpertise_stories');
